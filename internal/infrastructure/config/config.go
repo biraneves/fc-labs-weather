8dcf5d2cb5e0425212738de73bd37183a8c3d514
Ubirajara Neves <ice0311@infocert.it>
2025-10-10 18:23:24 +0200

    chore: :heavy_minus_sign: remove viper dependency

--- internal/infrastructure/config/config.go
+++ internal/infrastructure/config/config.go
@@ -4,11 +4,18 @@ import (
 	"errors"
 	"fmt"
 	"log/slog"
-	"path/filepath"
+	"os"
 	"strings"
 	"time"
 
-	"github.com/spf13/viper"
+	"github.com/joho/godotenv"
+)
+
+const (
+	defaultPort          = "8080"
+	defaultTimeout       = 5 * time.Second
+	defaultAPIReturnType = "json"
+	defaultWeatherAPIKey = "default_key"
 )
 
 type HTTPConfig struct {
@@ -35,54 +42,60 @@ type AppConfig struct {
 }
 
 func Load(dir string) (AppConfig, error) {
-	v := viper.New()
-
-	v.SetConfigFile(filepath.Join(dir, ".env"))
-	v.SetConfigType("env")
-	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-	v.AutomaticEnv()
-
-	if err := v.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			slog.Warn("config: .env file not found, relying on environment variables and defaults")
-		} else {
-			return AppConfig{}, fmt.Errorf("load config file: %w", err)
-		}
+	dotEnvFile := "./.env"
+	if err := godotenv.Load(dotEnvFile); err != nil {
+		slog.Warn("unable to read .env and relying on environment variables")
 	}
 
-	httpTimeout, err := parseDuration(v.GetString("HTTP_TIMEOUT"))
+	appPort := os.Getenv("PORT")
+	if appPort == "" {
+		slog.Warn("invalid PORT:", "default_value", defaultPort)
+		appPort = defaultPort
+	}
+
+	httpTimeout, err := parseDuration(os.Getenv("HTTP_TIMEOUT"))
 	if err != nil {
-		return AppConfig{}, fmt.Errorf("invalid HTTP_TIMEOUT: %w", err)
+		slog.Warn("invalid HTTP_TIMEOUT:", "default_value", defaultTimeout)
+		httpTimeout = defaultTimeout
 	}
 
-	viaCEPTimeout, err := parseDuration(v.GetString("VIACEP_TIMEOUT"))
+	viaCEPTimeout, err := parseDuration(os.Getenv("VIACEP_TIMEOUT"))
 	if err != nil {
-		return AppConfig{}, fmt.Errorf("invalid VIACEP_TIMEOUT: %w", err)
+		slog.Warn("invalid VIACEP_TIMEOUT:", "default_value", defaultTimeout)
+		httpTimeout = defaultTimeout
 	}
 
-	weatherTimeout, err := parseDuration(v.GetString("WEATHER_TIMEOUT"))
+	weatherTimeout, err := parseDuration(os.Getenv("WEATHER_TIMEOUT"))
 	if err != nil {
-		return AppConfig{}, fmt.Errorf("invalid WEATHER_TIMEOUT: %w", err)
+		slog.Warn("invalid WEATHER_TIMEOUT:", "default_value", defaultTimeout)
+		weatherTimeout = defaultTimeout
 	}
 
-	apiKey := strings.TrimSpace(v.GetString("WEATHER_API_KEY"))
-	if apiKey == "" {
-		return AppConfig{}, errors.New("missing WEATHER_API_KEY")
+	returnType := os.Getenv("VIACEP_RETURN_TYPE")
+	if returnType == "" {
+		slog.Warn("invalid VIACEP_RETURN_TYPE:", "default_value", defaultAPIReturnType)
+		returnType = defaultAPIReturnType
+	}
+
+	weatherAPIKey := os.Getenv("WEATHER_API_KEY")
+	if weatherAPIKey == "" {
+		slog.Warn("invalid WEATHER_API_KEY - using default value")
+		weatherAPIKey = defaultWeatherAPIKey
 	}
 
 	cfg := AppConfig{
 		HTTP: HTTPConfig{
-			Addr:    fmt.Sprintf(":%s", v.GetString("PORT")),
+			Addr:    fmt.Sprintf(":%s", appPort),
 			Timeout: httpTimeout,
 		},
 		ViaCEP: ViaCEPConfig{
-			BaseURL:    strings.TrimSuffix(v.GetString("VIACEP_URL"), "/"),
-			ReturnType: v.GetString("VIACEP_RETURN_TYPE"),
+			BaseURL:    strings.TrimSuffix(os.Getenv("VIACEP_URL"), "/"),
+			ReturnType: returnType,
 			Timeout:    viaCEPTimeout,
 		},
 		Weather: WeatherAPIConfig{
-			BaseURL: strings.TrimSuffix(v.GetString("WEATHER_URL"), "/"),
-			APIKey:  apiKey,
+			BaseURL: strings.TrimSuffix(os.Getenv("WEATHER_URL"), "/"),
+			APIKey:  weatherAPIKey,
 			Timeout: weatherTimeout,
 		},
 	}
